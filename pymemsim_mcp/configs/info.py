04b212d5b67b514ec03efecf4f9220607dbd1bf5
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-04-30 22:14:06 -0400

    init pkg

---
+++ pymemsim_mcp/configs/info.py
@@ -0,0 +1,11 @@
+# PACKAGE INFO
+# --------------
+
+# package name
+__package__ = 'pymemsim-mcp'
+# version
+__version__ = '0.1.0'
+# description
+__description__ = 'PyMemSim-MCP is an MCP-enabled extension of PyMemSim that integrates model-source-driven thermodynamic data with membrane process simulations.It enables LLMs and agents to interact with membrane models through standardized, machine-readable inputs, ensuring unit consistency, reproducibility, and reliable AI-assisted workflows.'
+# author
+__author__ = 'Sina Gilassi'
