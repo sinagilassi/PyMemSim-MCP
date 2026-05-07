0d7cd00e573440219f7014f6af681b9d92c82de2
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-05-07 13:58:11 -0400

    Bump version to 0.1.1 in package metadata

--- pymemsim_mcp/configs/info.py
+++ pymemsim_mcp/configs/info.py
@@ -4,7 +4,7 @@
 # package name
 __package__ = 'pymemsim-mcp'
 # version
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 # description
 __description__ = 'PyMemSim-MCP is an MCP-enabled extension of PyMemSim that integrates model-source-driven thermodynamic data with membrane process simulations.It enables LLMs and agents to interact with membrane models through standardized, machine-readable inputs, ensuring unit consistency, reproducibility, and reliable AI-assisted workflows.'
 # author
