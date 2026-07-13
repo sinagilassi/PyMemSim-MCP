5022fc84d213825a24e385c2ec2038d0e685c437
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-07-13 13:42:39 -0400

    Update package version to 0.4.0 in info.py and pyproject.toml

--- pymemsim_mcp/configs/info.py
+++ pymemsim_mcp/configs/info.py
@@ -4,7 +4,7 @@
 # package name
 __package__ = 'pymemsim-mcp'
 # version
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 # description
 __description__ = 'PyMemSim-MCP is an MCP-enabled extension of PyMemSim that integrates model-source-driven thermodynamic data with membrane process simulations.It enables LLMs and agents to interact with membrane models through standardized, machine-readable inputs, ensuring unit consistency, reproducibility, and reliable AI-assisted workflows.'
 # author
